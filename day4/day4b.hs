b7339da26faf0a54a3d9521f4fceb4cbf609245f
LilleLink <simon_0_0_@live.se>
2021-12-05 23:14:12 +0100

    Adds first version of the answer to day 4 in haskell

--- day4/day4b.hs
+++ day4/day4b.hs
@@ -6,11 +6,11 @@ main = do
     instructions <- map (\s -> read s :: Int) . segments (/=',') <$> readFile "instructions.txt"
     boards       <- map (map (map (\s -> read s :: Int) . words)) . segments (/="") . lines <$> readFile "boards.txt"
     let draws     = zip (map (\b -> nrOfDrawsToWin b (transpose b) instructions) boards) boards
-    let minDraws  = getMinDraws (map fst draws)
+    let minDraws  = getMaxDraws (map fst draws)
     let draws     = fst minDraws
     let minboard  = take 5 $ map snd (snd minDraws)
     let unmarked  = filter (\s -> s `notElem` take draws instructions) (concat minboard)
-    print $ sum unmarked * last (take draws instructions) -- draws * sum unmarked
+    print $ sum unmarked * last (take draws instructions)
 
 segments :: (a -> Bool) -> [a] -> [[a]]
 segments _ [] = []
@@ -25,10 +25,10 @@ nrOfDrawsToWin rs cs is = (minimum $ map fst calcedBoard, calcedBoard)
             | otherwise = checkRow is (p + 1) r
         calcedBoard = rowsToScore rs is ++ rowsToScore cs is
 
-getMinDraws :: [(Int, [a])] -> (Int, [a])
-getMinDraws bs = minboard bs (head bs)
+getMaxDraws :: [(Int, [a])] -> (Int, [a])
+getMaxDraws bs = maxboard bs (head bs)
     where
-        minboard [] currentMin         = currentMin
-        minboard ((i,b):bs) currentMin | fst currentMin > i = minboard bs (i,b)
-                                       | otherwise          = minboard bs currentMin
+        maxboard [] currentMax         = currentMax
+        maxboard ((i,b):bs) currentMax | fst currentMax < i = maxboard bs (i,b)
+                                       | otherwise          = maxboard bs currentMax
 

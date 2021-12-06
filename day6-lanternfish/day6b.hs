1f0ae65d9474e3cccda4854d72d4670d755d625f
LilleLink <simon_0_0_@live.se>
2021-12-06 15:39:13 +0100

    Refactors segments to splitOn using the split package

--- day6-lanternfish/day6b.hs
+++ day6-lanternfish/day6b.hs
@@ -1,10 +1,11 @@
 module Main where
+import Data.List.Split ( splitOn )
 
 type Fish = Int
 
 main :: IO ()
 main = do
-    content <- map (\s -> read s :: Fish) . segments (/=',') <$> readFile "input.txt"
+    content <- map (\s -> read s :: Fish) . splitOn "," <$> readFile "input.txt"
     putStr "Enter number of simulation days > "
     n <- (\s -> read s :: Int) <$> getLine
     let fishes = map (\s -> length $ filter (==s) content) [0..8]
@@ -12,8 +13,4 @@ main = do
 
 simulateDay :: [Fish] -> [Fish]
 simulateDay []            = []
-simulateDay fishes@(f:fs) = take 5 fs ++ [fs !! 5] ++ [fs !! 6 + f] ++ [fs !! 7] ++ [f] 
-
-segments :: (a -> Bool) -> [a] -> [[a]]
-segments _ [] = []
-segments p xs = takeWhile p xs : segments p (drop 1 $ dropWhile p xs)
+simulateDay fishes@(f:fs) = take 6 fs ++ [fs !! 6 + f] ++ [fs !! 7] ++ [f] 

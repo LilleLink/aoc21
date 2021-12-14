e218ef8f8a94d4857b25d56f117dbaf1c622087c
LilleLink <simon_0_0_@live.se>
2021-12-14 14:05:41 +0100

    Adds solution to day 8 part 2 in haskell

--- day8-seven-segment-search/day8b.hs
+++ day8-seven-segment-search/day8b.hs
@@ -1,5 +1,6 @@
 module Main where
 import Data.List.Split (splitOn)
+import Data.List ( (\\) )
 
 -- 0 = 6
 -- 1 = 2 - unique
@@ -12,38 +13,56 @@ import Data.List.Split (splitOn)
 -- 8 = 7 - unique
 -- 9 = 6
 
--- The rest of the numbers contain 5/6 segments respectively
+-- Length 2 = 1
+-- Length 3 = 7
+-- Length 4 = 4
+-- Length 5 = 2, 3, 5
+-- Length 6 = 0, 6, 9
+-- Length 7 = 8
 
--- Segments: 
--- top          - the char that doesnt exist in 1 but exists in 7
--- right side   - chars that exist in the union of 7 and 1
--- middle and top left - chars that exist in 4 but not in 7 except the top segment
+-- For lengths of 6:
+-- length 0 \ 1 = 4
+-- length 6 \ 1 = 5
+-- length 9 \ 1 = 4
 
--- this is all we can deduce from the digits with unique segment lengths
--- all of these in union form the 9
+-- length 0 \ 4 = 3
+-- length 9 \ 4 = 2
 
--- Example
--- be cfbegad cbdgef fgaecd cgeb fdcge agebfd fecdb fabcd edb
---  dddd
---      e
--- c?   e
---      e
---   g?
---      b
--- f?   b
---      b
---   a?
+-- For 2, 3, 5
+-- length 2 \ 4 = 3
+-- length 3 \ 4 = 2
+-- length 5 \ 4 = 2
 
--- be       = 1
--- cgeb     = 4
--- edb      = 7
--- cfbegad  = 8
+-- length 3 \ 1 = 3
+-- length 5 \ 1 = 4
 
+type Segment = (String, String)
 
 main :: IO ()
 main = do
     contents <- map (map words . splitOn " | ") . lines <$> readFile "input.txt"
     let outputs = [last x | x <- contents]
-    let uniquelengths = [2,3,4,7]
-    let lengths = length $ concatMap (filter (`elem` uniquelengths) . map length) outputs
-    print lengths
+    let inputs  = [head x | x <- contents]
+    let calculated =  map ((read :: String -> Int) . (\s -> calcOutputs (deduce $ head s) (last s))) contents
+    print $ sum calculated
+
+deduce :: [String] -> [Segment]
+deduce strings = [(zero,"0"),(one,"1"), (two,"2"), (three,"3"), (four,"4"), (five,"5"), (six,"6"), (seven,"7"), (eight,"8"), (nine,"9")]
+    where
+        zero  = head (filter (\s -> length s == 6 && length (s \\ one) == 4 && length (s \\ four) == 3) strings)
+        one   = head (filter (\s -> length s == 2) strings)
+        two   = head (filter (\s -> length s == 5 && length (s \\ four) == 3) strings)
+        three = head (filter (\s -> length s == 5 && length (s \\ four) == 2 && length (s \\ one) == 3) strings)
+        four  = head (filter (\s -> length s == 4) strings)
+        five  = head (filter (\s -> length s == 5 && length (s \\ four) == 2 && length (s \\ one) == 4) strings)
+        six   = head (filter (\s -> length s == 6 && length (s \\ one) == 5) strings)
+        seven = head (filter (\s -> length s == 3) strings)
+        eight = head (filter (\s -> length s == 7) strings)
+        nine  = head (filter (\s -> length s == 6 && length (s \\ one) == 4 && length (s \\ four) == 2) strings)
+
+calcOutputs :: [Segment] -> [String] -> String
+calcOutputs segments = concatMap getDigit
+    where
+        getDigit :: String -> String
+        getDigit d = snd $ head $ filter (\(s,digit) -> all (`elem` s) d && length s == length d) segments
+
